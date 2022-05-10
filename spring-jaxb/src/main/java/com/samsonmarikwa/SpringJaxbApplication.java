29ff8b9aad68311f68faa571c2e38c72be679fac
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-10 18:13:26 -0400

    spring jaxb project

---
+++ spring-jaxb/src/main/java/com/samsonmarikwa/SpringJaxbApplication.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJaxbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJaxbApplication.class, args);
+	}
+
+}
