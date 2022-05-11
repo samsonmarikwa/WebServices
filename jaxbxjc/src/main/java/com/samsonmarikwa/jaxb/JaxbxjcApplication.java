8f690e32831bdcb7ab105727065af4a272b8aa2a
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-11 13:03:55 -0400

    Create project, POM dependencies, class generation from XSD

---
+++ jaxbxjc/src/main/java/com/samsonmarikwa/jaxb/JaxbxjcApplication.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa.jaxb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxbxjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxbxjcApplication.class, args);
+	}
+
+}
---
+++ jaxbxjc/src/test/java/com/samsonmarikwa/jaxb/JaxbxjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa.jaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxbxjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
