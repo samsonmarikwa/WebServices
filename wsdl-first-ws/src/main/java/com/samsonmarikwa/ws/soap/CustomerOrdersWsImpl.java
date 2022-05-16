7a0a9da8d45616222035316b1cadfb79920cb7e9
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-15 23:02:57 -0400

    Create in-memory database using map

--- wsdl-first-ws/src/main/java/com/samsonmarikwa/ws/soap/CustomerOrdersWsImpl.java
+++ wsdl-first-ws/src/main/java/com/samsonmarikwa/ws/soap/CustomerOrdersWsImpl.java
@@ -1,12 +1,45 @@
 package com.samsonmarikwa.ws.soap;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.samsonmarikwa.CreateOrdersRequest;
 import com.samsonmarikwa.CreateOrdersResponse;
 import com.samsonmarikwa.CustomerOrdersPortType;
 import com.samsonmarikwa.GetOrdersRequest;
 import com.samsonmarikwa.GetOrdersResponse;
+import com.samsonmarikwa.Order;
+import com.samsonmarikwa.Product;
 
 public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
+	
+	// Create in-memory database 
+	Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
+	int currentId;
+	
+	public CustomerOrdersWsImpl() {
+		init();
+	}
+
+	public void init() {
+		List<Order> orders = new ArrayList<>();
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(1));
+		
+		Product product = new Product();
+		product.setId("1");
+		product.setDescription("iPhone");
+		product.setQuantity(BigInteger.valueOf(3));
+		
+		order.getProduct().add(product);
+		orders.add(order);
+		
+		customerOrders.put(BigInteger.valueOf(++currentId), orders);
+	}
+	
 
 	@Override
 	public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
