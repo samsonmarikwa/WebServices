e97ac9c87260fae183bb3d38fe322d8f292044db
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-15 22:48:29 -0400

    Create class to implement PortType Interface

---
+++ wsdl-first-ws/src/main/java/com/samsonmarikwa/ws/soap/CustomerOrdersWsImpl.java
@@ -0,0 +1,23 @@
+package com.samsonmarikwa.ws.soap;
+
+import com.samsonmarikwa.CreateOrdersRequest;
+import com.samsonmarikwa.CreateOrdersResponse;
+import com.samsonmarikwa.CustomerOrdersPortType;
+import com.samsonmarikwa.GetOrdersRequest;
+import com.samsonmarikwa.GetOrdersResponse;
+
+public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
+
+	@Override
+	public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
