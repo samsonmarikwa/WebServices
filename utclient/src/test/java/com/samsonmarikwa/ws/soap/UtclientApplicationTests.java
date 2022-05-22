3bc93806bc978c056323611ba5ca2b71e14b3ba0
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-21 22:30:33 -0400

    Create Username Token Project

---
+++ utclient/src/test/java/com/samsonmarikwa/ws/soap/UtclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsonmarikwa.ws.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
