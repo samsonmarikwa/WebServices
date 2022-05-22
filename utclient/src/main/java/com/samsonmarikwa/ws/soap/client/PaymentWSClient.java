d78b33f0001469ce2583a3275a63fd968722cf55
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-21 23:18:45 -0400

    Code the client

---
+++ utclient/src/main/java/com/samsonmarikwa/ws/soap/client/PaymentWSClient.java
@@ -0,0 +1,28 @@
+package com.samsonmarikwa.ws.soap.client;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.bharath.ws.soap.PaymentProcessor;
+import com.bharath.ws.soap.PaymentProcessorImplService;
+import com.bharath.ws.soap.PaymentProcessorRequest;
+import com.bharath.ws.soap.PaymentProcessorResponse;
+
+public class PaymentWSClient {
+
+	public static void main(String[] args) {
+		try {
+			PaymentProcessorImplService service = new PaymentProcessorImplService(
+					new URL("http://localhost:8080/javafirstws/paymentProcessor?wsdl"));
+			PaymentProcessor port = service.getPaymentProcessorImplPort();
+			
+			PaymentProcessorResponse response = port.processPayment(new PaymentProcessorRequest());
+			System.out.println(response.isResult());
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
