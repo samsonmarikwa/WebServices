a5df6eb9a7665d18cbd874d67b4fc5d1ce6eb0a2
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-22 22:11:22 -0400

    Implement the handleMessage method

--- wsdl-first-ws/src/main/java/com/samsonmarikwa/ws/soap/handlers/SiteHandler.java
+++ wsdl-first-ws/src/main/java/com/samsonmarikwa/ws/soap/handlers/SiteHandler.java
@@ -1,8 +1,14 @@
 package com.samsonmarikwa.ws.soap.handlers;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.Node;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
@@ -11,7 +17,20 @@ public class SiteHandler implements SOAPHandler<SOAPMessageContext> {
 
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
-		// TODO Auto-generated method stub
+		Boolean isResponse = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY); // if true, it is a response, if false it is a request
+		if(!isResponse) {
+			SOAPMessage soapMsg = context.getMessage();
+			try {
+				SOAPEnvelope envelope = soapMsg.getSOAPPart().getEnvelope();
+				SOAPHeader header = envelope.getHeader();
+				Iterator<Node> childElements = header.getChildElements();
+				
+			} catch (SOAPException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Response on the way");
+		}
 		return false;
 	}
 
