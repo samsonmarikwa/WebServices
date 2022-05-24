a668aa4f9f1af273bbc15b6b0dea11d9a23ca084
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-23 20:56:51 -0400

    Create and throw a custom exception

--- javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessorImpl.java
@@ -2,15 +2,16 @@ package com.bharath.ws.soap;
 
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+import com.bharath.ws.soap.exceptions.ServiceException;
 
 public class PaymentProcessorImpl implements PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) {
+	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest) throws ServiceException {
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
 		// Fault code
 		if (paymentProcessorRequest.getCreditCardInfo().getCardNumber() == null
 				|| paymentProcessorRequest.getCreditCardInfo().getCardNumber().length() == 0) {
-			throw new RuntimeException("Card number cannot be empty");
+			throw new ServiceException("Card number cannot be empty");
 		}
 		// Business Logic or a call to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
