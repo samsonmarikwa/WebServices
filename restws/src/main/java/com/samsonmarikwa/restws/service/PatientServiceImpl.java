7c88e9e3a84489e6319317267a841092840b694a
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-26 00:18:07 -0400

    create crud REST endpoints

--- restws/src/main/java/com/samsonmarikwa/restws/service/PatientServiceImpl.java
+++ restws/src/main/java/com/samsonmarikwa/restws/service/PatientServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.core.Response;
+
 import org.springframework.stereotype.Service;
 
 import com.samsonmarikwa.restws.model.Patient;
@@ -31,6 +33,49 @@ public class PatientServiceImpl implements PatientService {
 	public List<Patient> getPatients() {
 		Collection<Patient> results = patients.values();
 		List<Patient> response = new ArrayList<>(results);
+		return response;
+	}
+
+	@Override
+	public Patient getPatient(Long id) {
+		return patients.get(id);
+	}
+
+	@Override
+	public Response createPatient(Patient patient) {
+		patient.setId(++currentId);
+		patients.put(patient.getId(), patient);
+		
+		return Response.ok(patient).build();
+	}
+
+	@Override
+	public Response updatePatient(Patient patient) {
+		
+		Patient currentPatient = patients.get(patient.getId());
+		
+		Response response = null;
+		if (currentPatient != null) {
+			patients.put(patient.getId(), patient);
+			response = Response.ok().build();
+		} else {
+			response = Response.notModified().build();
+		}
+		return response;
+	}
+
+	@Override
+	public Response deletePatient(Long id) {
+		
+		Patient currentPatient = patients.get(id);
+		
+		Response response = null;
+		if (currentPatient != null) {
+			patients.remove(id);
+			response = Response.ok().build();
+		} else {
+			response = Response.notModified().build();
+		}
 		return response;
 	}
 	
