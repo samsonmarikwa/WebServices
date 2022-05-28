ef2175c0edb38640b7c59d157323abacc691e55f
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-28 00:47:54 -0400

    refactor and add other http methods

--- restwsclient/src/main/java/com/samsonmarikwa/restwsclient/PatientWSClient.java
+++ restwsclient/src/main/java/com/samsonmarikwa/restwsclient/PatientWSClient.java
@@ -4,37 +4,52 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.samsonmarikwa.restwsclient.model.Patient;
 
 public class PatientWSClient {
 
+	private static final String PATIENTS = "/patients";
+	private static final String PATIENT_SERVICE_URL = "http://localhost:8080/restws/services/patientservice";
+
 	public static void main(String[] args) {
 		
 		Client client = ClientBuilder.newClient();
-		
-		WebTarget target = client.target("http://localhost:8080/restws/services/patientservice/patients/123");
-		
+		WebTarget target = client.target(PATIENT_SERVICE_URL).path(PATIENTS).path("/{id}").resolveTemplate("id", 123);	
 		Builder request = target.request();
 		
-		//Response response = request.get();
-		
+		//Response response = request.get();	
 		// System.out.println(response.getStatus());
 		
-		Patient patient = request.get(Patient.class);
-		
+		// Get a patient
+		Patient patient = request.get(Patient.class);	
 		System.out.println("===============");
 		System.out.println(patient);
 		
+		// Update Patient
+		patient.setName("Annet Leonard");
+		WebTarget putTarget = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
+		Response updateResponse = putTarget.request().put(Entity.entity(patient, MediaType.APPLICATION_XML));
+		System.out.println(updateResponse.getStatus());
+		
+		// Create new patient
+		Patient newPatient = new Patient();
+		newPatient.setName("Tadisa Kanakembizi");
+		WebTarget postTarget = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
+		Patient createdPatient = postTarget.request().post(Entity.entity(newPatient, MediaType.APPLICATION_XML), Patient.class);
+		System.out.println("===============");
+		System.out.println(createdPatient);
+		
 		// Get list of patients
-		target = client.target("http://localhost:8080/restws/services/patientservice/patients");
+		target = client.target(PATIENT_SERVICE_URL).path(PATIENTS);
 		request = target.request();
 		List<Patient> patients = request.get(new GenericType<List<Patient>>() {});
-		
 		System.out.println("===============");
 		System.out.println(patients);
 	}
