ac1690b6176cfd718cece08ad4a6738658eaed4c
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-29 18:29:25 -0400

    Implement the Async method and return the responce using response.resume(response_value)

--- restwsasync/src/main/java/com/samsonmarikwa/restws/CheckProcessor.java
+++ restwsasync/src/main/java/com/samsonmarikwa/restws/CheckProcessor.java
@@ -2,6 +2,8 @@ package com.samsonmarikwa.restws;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 import com.samsonmarikwa.restws.model.ChecksList;
 
@@ -10,6 +12,6 @@ public interface CheckProcessor {
 	
 	@POST
 	@Path("/checks")
-	public Boolean processChecks(ChecksList checksList);	// a wrapper class has been used to wrap a list of checks. This is a good practice.
+	public void processChecks(@Suspended AsyncResponse response, ChecksList checksList);	// a wrapper class has been used to wrap a list of checks. This is a good practice.
 
 }
--- restwsasync/src/main/java/com/samsonmarikwa/restws/CheckProcessorImpl.java
+++ restwsasync/src/main/java/com/samsonmarikwa/restws/CheckProcessorImpl.java
@@ -1,12 +1,15 @@
 package com.samsonmarikwa.restws;
 
+import javax.ws.rs.container.AsyncResponse;
+
 import com.samsonmarikwa.restws.model.ChecksList;
 
 public class CheckProcessorImpl implements CheckProcessor {
 
 	@Override
-	public Boolean processChecks(ChecksList checksList) {
-		return null;
+	public void processChecks(AsyncResponse response, ChecksList checksList) {
+		//logic
+		response.resume(true);
 	}
 
 }
