d4a1fbd84767694d73d6f857b652addb684a47a2
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-30 22:23:54 -0400

    Code the download endpoint

--- restattachments/src/main/java/com/samsonmarikwa/restws/FileService.java
+++ restattachments/src/main/java/com/samsonmarikwa/restws/FileService.java
@@ -8,8 +8,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.springframework.stereotype.Service;
@@ -26,6 +29,15 @@ public class FileService {
 		for (Attachment attachment : attachments) {
 			copyFile(attachment.getDataHandler().getInputStream());
 		}
+	}
+	
+	@Path("/download")
+	@GET
+	public Response download() {
+		File file = new File(FILE_PATH);
+		ResponseBuilder responseBuilder = Response.ok(file);
+		responseBuilder.header("Content-Disposition", "attachment;filename=downloaded.jpg");
+		return responseBuilder.build();
 	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
