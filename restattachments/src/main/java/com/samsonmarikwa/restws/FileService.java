a436c7bfa78c0fdc0dc1d89af73c6753d209d391
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-30 21:45:33 -0400

    Create REST attachment client, add cxf client dependency

--- restattachments/src/main/java/com/samsonmarikwa/restws/FileService.java
+++ restattachments/src/main/java/com/samsonmarikwa/restws/FileService.java
@@ -6,10 +6,27 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.springframework.stereotype.Service;
+
+@Path("/fileService")
+@Service
 public class FileService {
 
-	private static final String FILE_PATH = "";
+	private static final String FILE_PATH = "C:\\CognizantLive\\WebServicesFiles\\uploaded\\uploaded.jpg";
+	
+	@Path("/upload")
+	@POST
+	public void upload(List<Attachment> attachments) throws FileNotFoundException, IOException {
+		for (Attachment attachment : attachments) {
+			copyFile(attachment.getDataHandler().getInputStream());
+		}
+	}
 
 	private void copyFile(InputStream inputStream) throws FileNotFoundException, IOException {
 		OutputStream out = null;
