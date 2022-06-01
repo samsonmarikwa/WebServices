fd1690d9a4cec8a5b111d48641538ed0cbd5e457
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-31 20:53:49 -0400

    create maven project

---
+++ jerseyws/src/main/java/com/samsonmarikwa/jerseyws/MyResource.java
@@ -0,0 +1,16 @@
+package com.samsonmarikwa.jerseyws;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("myresource")
+public class MyResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Jersey!";
+    }
+}
---
+++ jerseyws/target/jerseyws/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+    <h2>Jersey RESTful Web Application!</h2>
+    <p><a href="restapi/myresource">Jersey resource</a>
+    <p>Visit <a href="restapi/myresource">My Resource</a>
+</body>
+</html>
