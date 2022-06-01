4428519c72a24d5cb40b32ae97d58a5aa5a70eb4
Samson Marikwa <samsonmarikwa@outlook.com>
2022-05-31 23:04:03 -0400

    Create service interface, mark with JAX-RS annotation for HTTP methods and support of json

---
+++ productcrud/src/main/java/com/samsonmarikwa/rest/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.samsonmarikwa.rest.service;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.samsonmarikwa.rest.entities.Product;
+
+@Consumes("application/json")
+@Produces("application/json")
+@Path("/productservice")
+public interface ProductService {
+	
+	@Path("/products")
+	@GET
+	List<Product> getProduct();
+	
+	@Path("/products/{id}")
+	@GET
+	Product getProduct(@PathParam("id") int id);
+	
+	@Path("/products")
+	@POST
+	Response createProduct(Product product);
+	
+	@Path("/products")
+	@PUT
+	Response updateProduct(Product product);
+	
+	@Path("/products/{id}")
+	@DELETE
+	int deleteProduct(@PathParam("id") int id);
+}
---
+++ productcrud/src/main/java/com/samsonmarikwa/rest/service/ProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.samsonmarikwa.rest.service;
+
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import com.samsonmarikwa.rest.entities.Product;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public List<Product> getProduct() {
+		return null;
+	}
+
+	@Override
+	public Product getProduct(int id) {
+		return null;
+	}
+
+	@Override
+	public Response createProduct(Product product) {
+		return null;
+	}
+
+	@Override
+	public Response updateProduct(Product product) {
+		return null;
+	}
+
+	@Override
+	public int deleteProduct(int id) {
+		return 0;
+	}
+
+}
